efcda129b28002dda32904e04860ba660689a897
MohammedRagab00 <Mohammed_Ragab_@outlook.com>
2025-05-11 00:03:42 +0300

    Adding role seller

--- src/main/java/com/gotrid/trid/TriDApplication.java
+++ src/main/java/com/gotrid/trid/TriDApplication.java
@@ -5,6 +5,7 @@ import com.gotrid.trid.core.user.repository.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,15 +20,15 @@ public class TriDApplication {
         SpringApplication.run(TriDApplication.class, args);
     }
 
-//    @Bean
+    @Bean
     public CommandLineRunner runner(
             RoleRepository roleRepository
     ) {
         return args -> {
-            if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+            if (roleRepository.findByName("ROLE_SELLER").isEmpty()) {
                 roleRepository.save(
                         Role.builder()
-                                .name("ROLE_ADMIN")
+                                .name("ROLE_SELLER")
                                 .build()
                 );
             }
