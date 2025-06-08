05138575ffcfc35170b8a31c96c62776fa8a32d1
MohammedRagab00 <123463070+MohammedRagab00@users.noreply.github.com>
2025-06-08 15:07:01 +0300

    removed unused import in Cart

--- src/main/java/com/gotrid/trid/core/cart/model/Cart.java
+++ src/main/java/com/gotrid/trid/core/cart/model/Cart.java
@@ -1,22 +1,21 @@
     package com.gotrid.trid.core.cart.model;
 
-import com.gotrid.trid.common.model.AuditableEntity;
-import com.gotrid.trid.core.user.model.Users;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.OnDelete;
+    import com.gotrid.trid.common.model.AuditableEntity;
+    import com.gotrid.trid.core.user.model.Users;
+    import jakarta.persistence.CascadeType;
+    import jakarta.persistence.Entity;
+    import jakarta.persistence.OneToMany;
+    import jakarta.persistence.OneToOne;
+    import lombok.EqualsAndHashCode;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
+    import org.hibernate.annotations.OnDelete;
 
-import java.util.HashSet;
-import java.util.Set;
+    import java.util.HashSet;
+    import java.util.Set;
 
-import static org.hibernate.annotations.OnDeleteAction.CASCADE;
+    import static org.hibernate.annotations.OnDeleteAction.CASCADE;
 
 @Getter
 @Setter
